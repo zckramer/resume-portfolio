39b69ce25531425f93652a099f9930696e471db4
Zack Kramer <zckramer83@gmail.com>
2025-10-12 19:05:11 -0400

    refactor routes URLs

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,26 @@ import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
 import Bloggish from './components/Bloggish/Bloggish';
 
+const BASE_URL_ROUTE = '/resume-portfolio';
+const ROUTE_PATHS = {
+  HOME: BASE_URL_ROUTE + '/',
+  RESUME: BASE_URL_ROUTE + '/resume',
+  PORTFOLIO: BASE_URL_ROUTE + '/portfolio',
+  BLOGGISH: BASE_URL_ROUTE + '/bloggish',
+  CONTACT: BASE_URL_ROUTE + '/contact'
+}
+
 const App = () => {
   
   return (
     <>
       <NavBar/>
       <Routes>
-        <Route path="/" element={<Resume />} />
-        <Route path="/resume" element={<Resume/>} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/bloggish" element={<Bloggish />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path={ROUTE_PATHS.HOME} element={<Contact />} />
+        <Route path={ROUTE_PATHS.RESUME} element={<Resume/>} />
+        <Route path={ROUTE_PATHS.PORTFOLIO} element={<Portfolio />} />
+        <Route path={ROUTE_PATHS.BLOGGISH} element={<Bloggish />} />
+        <Route path={ROUTE_PATHS.CONTACT} element={<Contact />} />
       </Routes>
     </>
   )
